87f3047bd8b3e1da324ff226c828e980083351ad
luis <vnunes30@gmail.com>
2023-04-03 06:11:48 -0300

    problema de import de modules, nao resolvido , consigo fazer o test por erro de improt de modoulo

--- mongopycrud/main.py
+++ mongopycrud/main.py
@@ -3,21 +3,20 @@ from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router
 
-
+config= dotenv_values(".env")
 
 app = FastAPI()
-
+app.include_router(router, tags=["people"],prefix="/people")
 
 
 
 
 @app.on_event("startup")
 def statup_db_client():
-    app.mongodb_client = MongoClient("mongodb+srv://Luis:h0TtsxwuhRMXVSmo@clustertest.ilqkbh5.mongodb.net/?retryWrites=true&w=majority")
-    app.database = app.mongodb_client["test_mongo"]
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
 
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
 
-app.include_router(router, tags=["people"],prefix="/people")
